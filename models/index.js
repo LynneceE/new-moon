7cb489a53893820d21847114119cf4f05b2b848e
bhari eutsey <bharilynn@outlook.com>
2022-02-19 20:55:25 -0600

    post routes created and working correctly

--- models/index.js
+++ models/index.js
@@ -1,4 +1,19 @@
-const User = require('./User');
 const Post = require('./Post');
+const User = require('./User');
+
+
+
+// create associations
+
+//1 team many players
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//blue player belongs to blue team
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 
 module.exports = { User, Post };
