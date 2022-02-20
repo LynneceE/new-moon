3fd57c93d6706e56324bcae1ef137fbf5e5a840d
bhari eutsey <bharilynn@outlook.com>
2022-02-19 21:30:46 -0600

    comment model created

--- models/index.js
+++ models/index.js
@@ -1,19 +1,36 @@
 const Post = require('./Post');
 const User = require('./User');
+const Comment = require('./Comment');
 
 
 
 // create associations
 
-//1 team many players
+//nba has many teams
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
-//blue player belongs to blue team
+//this team belongs to nba
 Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
+//this player plays for nba
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+//this player plays for this team
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+//nba has many players
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+}),
+//this team has many players
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+}),
 
 
-module.exports = { User, Post };
+module.exports = { User, Post, Comment };
